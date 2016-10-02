b4ef87269b0af6f84a3ae58eb80b85d22fd414b6
kishore <kish.karthik11@gmail.com>
2016-10-02 11:19:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 
 //code to move the image
 
-var img= document.getElementById('modi');
+var img= document.getElementById('madi');
 img.onclick.function(){
     img.style.marginLeft='100px';
 }
