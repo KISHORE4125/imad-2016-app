e49217ba2d845b419000bbab88c72a98b92a8bbe
kishore <kish.karthik11@gmail.com>
2016-10-02 07:44:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article1',function(req,res){
+   rs.send('Article1 requseted')  
+});
+app.get('/article2',function(req,res){
+   rs.send('Article2 requseted')  
+});
+app.get('/article3',function(req,res){
+   rs.send('Article3 requseted')  
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
