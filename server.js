aa725dd0004770793aa38dd6adfa30398f679f96
kishore <kish.karthik11@gmail.com>
2016-10-02 07:45:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article1',function(req,res){
-   res.send('Article1 requseted')  
+   res.send('Article1 requseted') ; 
 });
 app.get('/article2',function(req,res){
-   res.send('Article2 requseted')  
+   res.send('Article2 requseted') ; 
 });
 app.get('/article3',function(req,res){
-   res.send('Article3 requseted')  
+   res.send('Article3 requseted') ; 
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
