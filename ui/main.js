c4f6052a04a0afc8a10de652b57689a1fd281194
kishore <kish.karthik11@gmail.com>
2016-10-07 07:36:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 
 //code to move the image
 
-var img= document.getElementById('img');
+var img= document.getElementById('madi');
 img.onclick = function(){        //onclick means execute a function on clicking the image
     img.style.marginLeft='100px';
 };
