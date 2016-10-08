11f7b4b7cafc6c75560cfd219a68812cd4be2b36
kishore <kish.karthik11@gmail.com>
2016-10-08 22:45:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,6 @@ console.log('Loaded!');
 //MOVE THE IMAGE
 
 var img=document.getElementByID('madi');
-var marginLeft=0;
-function movRight(){
-    marginLeft=marginLeft+5;
-    img.style.marginLeft=marginLeft+ 'px';
-    }
 img.onclick=function(){
-    var interval=setInterval(moveRight,50);
+    img.style.marginleft='100px';
 };
